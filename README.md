2d3eb14b26c6f5989e34e82cc9c7b91ebdda2471
Totooria <lucy@lucyvin.com>
2023-06-02 23:01:12 -0400

    add installer, readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# ytdlp-auth-download
+A wrapper script for yt-dlp designed to simplify use of yt-dlp with authentication-requiring sources and automation
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp ./ytdlp-auth-download.sh ~/.local/bin/ytdlp-auth-download
+chmod +x ~/.local/bin/ytdlp-auth-download
+echo "Install complete, please ensure ~/.local/bin is on your PATH"
